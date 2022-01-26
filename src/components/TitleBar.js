ed2ea8a7ddd7678254bf7183040cf60f52c53820
Keith Douglas <keith@keithdouglas.co.uk>
2022-01-26 15:11:56 +0000

    updates title again

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
-      <h1>Keith&apos;s Top Ten</h1>
+      <h1>Keith&apos;s Top Tenn</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
